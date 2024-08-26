28339c3be4d6ec9a7b7d7ae48e4388de749c8882
huzaifa <hrabnaw@gmail.com>
2024-08-26 21:52:42 +0500

    add some follder or file

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,7 +3,23 @@ const router=express.Router()
 const {signupvalidation}=require("../helpers/validation")
 const usercontrols=require("../controllers/usercontroler")
 
+const path=require("path")
+const multer=require("multer")
+
+const storage=multer.diskStorage({
+    destination:function(req,res,calbackfn){
+
+    },
+    filename:function(req,res,calbackfn){
+
+    }
+})
+
+const update=multer({storage:storage})
+
+
 router.post("/register",signupvalidation ,usercontrols.register)
+
 
 
 
